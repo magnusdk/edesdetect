203e24f4b484b5167f2e30629a1a62b4813d19a3
Magnus Dalen Kvalevåg <magnus.kvalevag@gmail.com>
2021-07-30 14:35:29 +0200

    Add integration test for CheckpointManager
    #1

--- edesdetectrl/util/checkpoints.py
+++ edesdetectrl/util/checkpoints.py
@@ -7,7 +7,7 @@ class CheckpointManager:
     """
     Utility class for creating and restoring checkpoints for training.
 
-    Currently only stores checkpoints for the model parameters and the training monitor, i.e.: not the sample buffer.
+    Saves and restores model parameters and training monitor counters.
     """
 
     def __init__(self, model, train_monitor, filepath):
@@ -15,7 +15,7 @@ class CheckpointManager:
         self.train_monitor = train_monitor
         self.filepath = filepath
 
-    def add_checkpoint(self):
+    def save_checkpoint(self):
         checkpointed_state = {
             "model_params": self.model.params,
             "train_monitor_counters": self.train_monitor.get_counters(),
@@ -28,9 +28,10 @@ class CheckpointManager:
             f.write(pickle.dumps(checkpointed_state))
 
     def restore_latest(self):
-        with lz4.frame.open(self.filepath, "rb") as f:
-            checkpointed_state = pickle.loads(f.read())
-            self.model.params = checkpointed_state["model_params"]
-            self.train_monitor.set_counters(
-                checkpointed_state["train_monitor_counters"]
-            )
+        if os.path.isfile(self.filepath):
+            with lz4.frame.open(self.filepath, "rb") as f:
+                checkpointed_state = pickle.loads(f.read())
+                self.model.params = checkpointed_state["model_params"]
+                self.train_monitor.set_counters(
+                    checkpointed_state["train_monitor_counters"]
+                )
