c5182bdb4d55a448c8a3e9e862a611424f0e5006
Magnus Dalen Kvalevåg <magnus.kvalevag@gmail.com>
2021-08-27 17:59:26 +0200

    Add Clojure-inspired threading-"macros"

---
+++ edesdetectrl/util/functional.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+
+def chainf(initial, *args):
+    """Chain functions and pseudo S-expressions like macro-thread-first from languages like Clojure, i.e.: `->`.
+
+    How this works is best understood by looking at examples. For examples see tests.
+    """
+    return reduce(lambda v, f: f(v) if callable(f) else f[0](*f[1:], v), args, initial)
+
+
+def chainl(initial, *args):
+    """Chain functions and pseudo S-expressions like macro-thread-last from languages like Clojure, i.e.: `->>`.
+
+    This function should only be used to chain generators.
+
+    How this works is best understood by looking at examples. For examples see tests."""
+    return reduce(lambda v, f: f(v) if callable(f) else f[0](*f[1:], v), args, initial)
---
+++ tests/edesdetectrl/util/test_functional.py
@@ -0,0 +1,54 @@
+import edesdetectrl.util.functional as func
+import operator as op
+import pytest
+
+
+def test_chainf_functions():
+    # `chainf` is a more readable way of chaining multiple function calls.
+    v = 1
+    f1 = lambda x: x + 3
+    f2 = lambda x: x ** 2
+    f3 = lambda x: x / 2
+    res = func.chainf(v, f1, f2, f3)
+    # This is the same as writing `f3(f2(f1(v)))`.
+    assert res == 8.0
+
+
+def test_chainf_sexprs():
+    # `chainf` also works with Lisp-like S-expressions.
+    # The values will be inserted as the first arguments to the S-expression functions with subsequent arguments coming last.
+    # For inserting the values at the end instead (for example when chaining generators), see `chainl`.
+    v = 1
+    f1 = (op.add, 3)  # S-expressions can be tuples
+    f2 = [op.pow, 2]  # ...or lists.
+    f3 = lambda x: x / 2  # But we can also mix in regular functions.
+    res = func.chainf(v, f1, f2, f3)
+    # This is the same as writing `f3(op.pow(op.add(v, 3), 2))`.
+    assert res == 8.0
+
+
+def test_chainl_sexprs():
+    # `chainl` is meant to chain generators together.
+    # It is very similar to `chainf`, but values will be inserted at the end of S-expressions instead of as the first argument.
+    g = range(5)
+    t1 = (map, lambda x: x * 3)  # S-expressions can be tuples
+    t2 = [filter, lambda x: x % 2 == 0]  # ...or lists.
+    res = func.chainl(g, t1, t2)
+    # This is the same as writing `filter(lambda x: x % 2 == 0, map(lambda x: x * 3, range(10)))`.
+    assert list(res) == [0, 6, 12]
+    # Semantically, the above statements says:
+    # 1. Take the sequence of numbers [0,5) (= [0,1,2,3,4])
+    # 2. Multiply each number by 3 (= [0,3,6,9,12])
+    # 3. Filter out all the odd numbers (= [0, 6, 12])
+    # 4. Assert that the result equals [0, 6, 12].
+
+    # Here's the same example in a (possibly) more readable format:
+    assert (
+        func.chainl(
+            range(5),
+            (map, lambda x: x * 3),
+            (filter, lambda x: x % 2 == 0),
+            list,
+        )
+        == [0, 6, 12]
+    )
